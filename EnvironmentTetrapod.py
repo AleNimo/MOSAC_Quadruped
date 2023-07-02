f4d992ea00822ab2149e40d18a70882c4d815087
Alejo D. Nimo <adomnguez@frba.utn.edu.ar>
2023-07-02 14:40:29 -0300

    Prueba con otro tipo de recompensa: Recompensa positiva por espalda horizontal, valor proporcional al avance hacia el target

--- EnvironmentTetrapod.py
+++ EnvironmentTetrapod.py
@@ -20,11 +20,11 @@ class Environment:
         self.__obs = np.zeros((1,)+self.obs_sp_shape)           # Observed state
         self.__coppelia = CoppeliaSocket(obs_sp_shape[0])       # Socket to the simulated environment
         
-        self.__step = 0                                         # Number of steps taken in the episode
-        self.__total_reward_mean = 0                            # Mean of all the rewards before adding penalization for moving violently
-        self.__reward_threshold = 1                             # Threshold at which penalization for moving violently begins (mean step of 0.01)
-        self.__K1 = -3.4                                        # To penalize proportionally to the excess of reward above the threshold if it moves violently
-        self.__K2 = -6.8                                        # To penalize a maximum amount if the excess of reward above the threshold is high enough
+        # self.__step = 0                                         # Number of steps taken in the episode
+        # self.__total_reward_mean = 0                            # Mean of all the rewards before adding penalization for moving violently
+        # self.__reward_threshold = 1                             # Threshold at which penalization for moving violently begins (mean step of 0.01)
+        # self.__K1 = -3                                       # To penalize proportionally to the excess of reward above the threshold if it moves violently
+        # self.__K2 = -9                                       # To penalize a maximum amount if the excess of reward above the threshold is high enough
 
     def reset(self):
         ''' Generates and returns a new observed state for the environment (outside of the termination condition) '''
@@ -82,35 +82,37 @@ class Environment:
             else:
                 reward[i], end[i] = 100*(dist_ini[i]-dist_fin[i]), False if dist_fin[i] <= 1.5 else True
             
-            #increase number of steps taken
-            self.__step += 1
+            # #increase number of steps taken
+            # self.__step += 1
 
-            #compute reward mean of all steps taken
-            self.__total_reward_mean = (self.__total_reward_mean * (self.__step - 1) + reward[i]) / self.__step
+            # #compute reward mean of all steps taken
+            # self.__total_reward_mean = (self.__total_reward_mean * (self.__step - 1) + reward[i]) / self.__step
             
-            #If the mean of the last 4 rewards based only on distance traveled to the target is:
-                # <= the reward threshold, the agent is not penalized for moving violently
-                # > the reward threshold, the agent is penalized for moving violently proportionally to the the excess of reward above the threshold (K1 * (r-thr))
-                # >> the reward threshold, the agent is penalized for moving violently with a maximum constant multiplied (K2).
+            # #If the mean of the last 4 rewards based only on distance traveled to the target is:
+            #     # <= the reward threshold, the agent is not penalized for moving violently
+            #     # > the reward threshold, the agent is penalized for moving violently proportionally to the the excess of reward above the threshold (K1 * (r-thr))
+            #     # >> the reward threshold, the agent is penalized for moving violently with a maximum constant multiplied (K2).
             
-            if end[i]:
-                print(self.__total_reward_mean)
-            
-            if self.__total_reward_mean >= self.__reward_threshold:
-            
+            # if self.__total_reward_mean >= self.__reward_threshold:
+            if reward[i] > 0:
                 #Get the 2 angles (x and y axes) of the back
-                # for j in range(3, 5):
-                #     back_angle = np.abs(next_obs[i, j])    #angle of the back with respect to 0°
-    
-                #     if back_angle > 1/9:  #if it exceeds 20°
-                #         reward[i] += max(self.__K1*(self.__total_reward_mean-self.__reward_threshold), self.__K2) * back_angle * 1/2    #Mean multiplied by negative constant
+                for j in range(3, 5):
+                    back_angle = np.abs(next_obs[i, j])    #angle of the back with respect to 0° (horizontal position)
+                    
+                    #if the angle is 0° the reward increases 50%, if it is 30° or more it is not increased (The mean of the X,Y angles is computed)
+                    if back_angle < 1/6:
+                        reward[i] += (1/6-back_angle)*3 * reward[i] * 1/2
     
                 #Get angular movement of the 12 joints
-                for joint in range(6, 18):
-                    delta_joint_angle = np.abs(obs[i, joint] - next_obs[i, joint])    #angle of every joint with respect to the previous one
+                # for joint in range(6, 18):
+                #     delta_joint_angle = np.abs(obs[i, joint] - next_obs[i, joint])    #angle of every joint with respect to the previous one
     
-                    if delta_joint_angle > 1/18: #if it exceeds 10°
-                        reward[i] += max(self.__K1*(self.__total_reward_mean-self.__reward_threshold), self.__K2) * delta_joint_angle * 1/12    #Mean multiplied by negative constant
+                #     if delta_joint_angle > 1/9: #if it exceeds 10°
+                #         reward[i] += max(self.__K1*(self.__total_reward_mean-self.__reward_threshold), self.__K2) * delta_joint_angle * 1/12    #Mean multiplied by negative constant
+            
+            #If the robot flips downwards the episode ends (absolute value of X or Y angle greater than 90°)
+            if abs(next_obs[i, 3]) >= 0.5 or abs(next_obs[i, 4]) >= 0.5:
+                end[i] = True
 
         return reward, end
 
@@ -142,6 +144,6 @@ if __name__ == '__main__':
     model.plot_resolution = 10
 
     # Start training
-    model.train(episodes=5000, ep_steps=100, save_period=1000, plot_period=50)
+    model.train(episodes=60000, ep_steps=100, save_period=1000, plot_period=50)
 
     model.test(ep_steps=100)
